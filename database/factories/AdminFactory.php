e23550ac218834eae4eedaa3ef2cec30c110c8f3
Kevin Canico <kevcanico25@gmail.com>
2021-06-20 19:05:22 +0200

    🌱 Add seed Admin

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -2,8 +2,9 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\User;
+use App\Admin;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /*
@@ -17,12 +18,12 @@ use Illuminate\Support\Str;
 |
 */
 
-$factory->define(User::class, function (Faker $faker) {
+$factory->define(Admin::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        'name' => 'admin',
+        'email' => 'admin@monkeys.com',
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => Hash::make('password'), // password
         'remember_token' => Str::random(10),
     ];
 });
